5a893547a10abd6809370b82bb378d9182ac6bd1
heyleydotdev <maxshashi29@gmail.com>
2024-11-29 23:52:44 +0530

    create todo

--- apps/contract/programs/contract/src/lib.rs
+++ apps/contract/programs/contract/src/lib.rs
@@ -6,11 +6,42 @@ declare_id!("9j59z5JAvp6XDvY7XhRJC9oPKg4WmEDWZa3bj4H4RdHq");
 pub mod contract {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_todo(ctx: Context<CreateToDoEntry>, id: Pubkey, content: String) -> Result<()> {
+        let entry = &mut ctx.accounts.todo_entry;
+
+        entry.id = id;
+        entry.user = *ctx.accounts.user.key;
+        entry.content = content;
+        
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(id: Pubkey)]
+pub struct CreateToDoEntry<'info> {
+    #[account(
+        init,
+        seeds = [b"todo_seeds", id.key().as_ref(), user.key().as_ref()],
+        bump,
+        payer = user,
+        space = DISCRIMINATOR + ToDoState::INIT_SPACE,
+    )]
+    pub todo_entry: Account<'info, ToDoState>,
+    
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct ToDoState {
+    pub id: Pubkey,
+    pub user: Pubkey,
+    #[max_len(20)]
+    pub content: String
+}
+
+const DISCRIMINATOR: usize = 8;
--- apps/contract/tests/contract.ts
+++ apps/contract/tests/contract.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { assert } from "chai";
 import { Contract } from "../target/types/contract";
 
 describe("contract", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Contract as Program<Contract>;
+  const provider = program.provider;
+  const programId = program.programId;
+  const wallet = (provider as unknown as { wallet: anchor.Wallet }).wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Create todo", async () => {
+    const user = wallet.payer;
+    const id = anchor.web3.Keypair.generate().publicKey;
+    const content = "first todo";
+
+    const [todoEntry] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("todo_seeds"), id.toBuffer(), user.publicKey.toBuffer()],
+      programId
+    );
+
+    await program.methods
+      .createTodo(id, content)
+      .accounts({ user: user.publicKey })
+      .signers([user])
+      .rpc();
+
+    const createdTodo = await program.account.toDoState.fetch(todoEntry);
+
+    assert.ok(createdTodo.id.equals(id));
+    assert.ok(createdTodo.user.equals(user.publicKey));
+    assert.ok(createdTodo.content === content);
   });
 });
